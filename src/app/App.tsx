56c7085b95103c831c3cbc71b8a8bcca4c0c499c
helinskimatheus <matheus.helinski@agenciam3.com>
2023-10-10 17:53:18 -0300

    feat(project): adicona estrutura inicial do projeto

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>aixijdi</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import App from "./app/App.tsx";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootElement)
+  .render(
+    <App />
+  );
